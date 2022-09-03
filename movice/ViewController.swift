9020441a715473c1d47f6bd4e3f5072b6918bd71
atsuki-uenishi <cacao3725@gmail.com>
2022-09-03 09:21:37 +0900

    Initial Commit

---
+++ movice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movice
+//
+//  Created by 上西篤季 on 2022/09/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
